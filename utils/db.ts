43d76c4c03dcf0764c811bea09709eaa5497efc7
suraj vishwakarma <dev.surajv@gmail.com>
2025-12-04 12:07:54 +0530

    feat: configure PrismaClient with logging options for improved debugging

--- utils/db.ts
+++ utils/db.ts
@@ -5,6 +5,9 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ 
+  adapter ,
+  log: ["query", "info", "warn", "error"] 
+});
 
 export default prisma;
